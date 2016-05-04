c3eae97ac9d6ac0a3a8644bd850478a1c593e815
Heidi Durhuus <heididurhuus@gmail.com>
2016-05-04 15:25:17 +0200

    added new_plant

---
+++ client/new_plant.js
@@ -0,0 +1,27 @@
+
+Template.new_plant.helpers({
+  plants:function(){
+    return Plants.find();
+  },
+  isSelected: function(){
+    return Session.get("isSelected");
+  },
+  hasImage: function(){
+    return false;
+  },
+  create: function(){
+
+  },
+  rendered: function(){
+
+  },
+  destroyed: function(){
+
+  },
+});
+
+Template.new_plant.events({
+  "change .js-plant-selected": function(event, template){
+    Session.set("isSelected", true);
+  }
+});
