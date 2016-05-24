db85431ed28862f21c737dc61912337b0e1df04a
Heidi Durhuus <heididurhuus@gmail.com>
2016-05-24 13:13:36 +0200

    entries array puts new entry first in the array

--- server/server.js
+++ server/server.js
@@ -23,7 +23,27 @@ Meteor.methods({
     return upd;
   },
   insertPlantlogEntry: function(plantlog_id, entry){
-    var ins = PlantLog.update({_id: plantlog_id}, {$push: {entries: entry}});
+    //inserts an object to the beginning of the array (unshift)
+    var ins = PlantLog.update({
+      _id: plantlog_id
+    }, {
+      $push: {
+        entries: {
+          $each: [entry],
+          $position: 0
+        }
+      }
+    });
+
+/*
+    var ins = PlantLog.update({
+      _id: plantlog_id
+    }, {
+      $push: {
+        entries: entry
+      }
+    });
+*/
   },
   setUserLanguage: function(lang){
     Meteor.users.update(this.userId, {$set: {language: lang}});
